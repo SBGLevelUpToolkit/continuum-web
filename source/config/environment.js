6648e8047a0ac2df8d03640efc56c2c9da313284
brett.upton <brett.upton@standardbank.co.za>
2015-10-30 14:36:56 +0200

    Testing environment switch

--- source/config/environment.js
+++ source/config/environment.js
@@ -1,8 +1,10 @@
 function getEnvironment() {
     let host = window.location.pathname;
     if (host.indexOf('staging') > -1 || host.indexOf('source') > -1) {
+        console.log('Host: ' + host);
         return 'http://continuumapi-staging.azurewebsites.net';
     } else {
+        console.log('Host: ' + host);
         return 'http://continuumapi.azurewebsites.net';
     }
 }
